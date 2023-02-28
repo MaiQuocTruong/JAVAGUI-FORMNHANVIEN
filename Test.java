f597799ac60c0fb77970925e2b2a0cdbee177ef5
maiquoctruong <nguyenthanhtung0900@gmail.com>
2023-02-28 16:19:07 +0700

    java gui

---
+++ Test.java
@@ -0,0 +1,10 @@
+package STT27_MaiQuocTruong_NhanVien;
+
+
+public class Test {
+	public static void main(String[] args){
+		DanhSachNhanVien dao=new DanhSachNhanVien();
+		new frnNhanVien(dao).setVisible(true);
+	}
+}
+
